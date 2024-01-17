e6af7a5dc8ee2e1e9f8ca5efe12fb5498aaa4f2b
saneknapenek <kolesnikovag22@gmail.com>
2024-01-18 01:13:35 +0300

    f

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from _logging import logger
 
 
 
@@ -10,4 +11,5 @@ class Mode(Enum):
 class CameraError(Exception):
     def __init__(self, message: str):
         self.message = message
+        logger.error(self.message)
         super().__init__(self.message)
